387f2afe09802d87fd4f668138c1d6619994d736
Per Rovegard <per@rovegard.se>
2011-12-20 00:08:23 +0100

    First commit

---
+++ CachingTest/CachingTest.fs
@@ -0,0 +1,22 @@
+﻿module CachingTest
+
+open NUnit.Framework
+open Caching
+
+[<TestFixture>]
+type cachingFixture() = class
+
+    [<Test>]
+    member self.cacheWithSameInputTest() =
+       let value1 = produce 1
+       let value2 = produce 1
+       Assert.AreSame (value1, value2)
+
+    [<Test>]
+    member self.dontCacheWithDifferentInputsTest() =
+       let value1 = produce 1
+       let value2 = produce 2
+       Assert.AreNotSame (value1, value2)
+
+end
+
---
+++ FSharpCaching/Caching.fs
@@ -0,0 +1,29 @@
+﻿module Caching
+
+let memoize f =
+    let dict = new System.Collections.Generic.Dictionary<_,_>()
+    fun n ->
+        match dict.TryGetValue(n) with
+        | (true, v) -> v
+        | _ ->
+            let temp = f(n)
+            dict.Add(n, temp)
+            temp
+
+type AType(i:int) = class end
+
+let produceWithArg i = 
+    new AType(i)
+//let produce = produceWithArg
+
+let produceNoArg =
+    fun i -> new AType(i)
+//let produce = produceNoArg
+
+let produceLazy i =
+    lazy(new AType(i))
+//let produce i = (produceLazy i).Force()
+
+let produceMem = memoize(fun i ->
+    new AType(i) )
+let produce = produceMem
